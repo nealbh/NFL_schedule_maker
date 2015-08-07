c2b69dc194c71fe350dee0978be521adb7d9dbe4
nealbh <neal.bhattarai@gmail.com>
2015-08-07 15:02:12 -0500

    Division games set

---
+++ NFL_schedule.java
@@ -0,0 +1,201 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+class NFL_schedule{
+	public static Team returnObjectByName(ArrayList a,String name){
+		Team iter = null;
+		for(int i = 0 ; i < a.size() ; ++i){
+			iter = (Team)a.get(i);
+			if(name.equals(iter.getName())) break;
+		}
+		return iter;
+	}
+
+	public static ArrayList init_teams(String city, String name, int prev, Scanner sc, ArrayList team, Team.Confrence conf, Team.Division div){
+		System.out.print("Where did the " +city + " "+ name + " finish in the " + conf +" "+ div + " in " + prev + " : ");
+		int stand = sc.nextInt();
+		while(stand < 1 || stand > 4){
+			System.out.print("Please enter a number between 1-4 where the "+name+" finished the season in "+prev+": ");
+			stand = sc.nextInt();
+		}
+		team.add(new Team(name, conf, div, stand));
+		return team;
+	}
+
+	public static void setByeWeek(ArrayList teams){
+		ArrayList<Team> bye_selection = new ArrayList<Team>();
+		bye_selection.addAll(teams);
+
+		for(int i = 0 ; i < teams.size(); ++i){
+			int bye_week = (i / 4) + 4;
+			Random rand = new Random();
+			int bye_team = rand.nextInt(bye_selection.size());
+			Team t = returnObjectByName(teams, bye_selection.get(bye_team).getName());
+			bye_selection.remove(bye_team);
+			String [] s = t.getSchedule();
+			// array zero based, schedule one based
+			s[bye_week-1] = "Bye";
+			t.setSchedule(s);
+			System.out.println(t.getName() + " are on a bye in Week " + bye_week);
+		}
+	}
+
+	public static void setGame(Team a , Team b, boolean isAHome, int week){
+		String [] aSch = a.getSchedule();
+		String [] bSch = b.getSchedule();
+		if(week == -1){
+			Random r = new Random();
+			week = r.nextInt(16);
+			while(aSch[week] != null || bSch[week] != null){
+				week = r.nextInt(16);
+			}
+		}
+		System.out.println(aSch[week]);
+		System.out.println(bSch[week]);
+		if(isAHome){
+			aSch[week] = b.getName();
+			bSch[week] = "@ " + a.getName();
+		} else {
+			aSch[week] = "@ " + b.getName();
+			bSch[week] = a.getName();
+		}
+		a.setSchedule(aSch);
+		b.setSchedule(bSch);
+	}
+
+	public static void setDivGame(ArrayList div){
+		Random rand = new Random();
+		boolean home = rand.nextBoolean();
+		int divOpp = rand.nextInt(3) + 1;
+	//	System.out.println(divOpp);
+	//	System.out.println(((Team)div.get(0)).getName() + "\t" +((Team)div.get(divOpp)).getName());
+		Team a = (Team)div.get(0);
+		Team b = (Team)div.get(divOpp);
+		div.remove(divOpp);
+		div.remove(0);
+		Team c = (Team)div.get(0);
+		Team d = (Team)div.get(1);
+		setGame(a, b, home, 16);
+		setGame(a, b, !home, -1);
+		home = rand.nextBoolean();
+	//	System.out.println(((Team)div.get(0)).getName() + "\t" +((Team)div.get(1)).getName());
+		setGame(c, d, home, 16);
+		setGame(c, d, !home, -1);
+		home = rand.nextBoolean();
+		setGame(a, c, home, -1);
+		setGame(a, c, !home, -1);
+		home = rand.nextBoolean();
+		setGame(a, d, home, -1);
+		setGame(a, d, !home, -1);
+		home = rand.nextBoolean();
+		setGame(b, c, home, -1);
+		setGame(b, c, !home, -1);
+		home = rand.nextBoolean();
+		setGame(b, d, home, -1);
+		setGame(b, d, !home, -1);
+	}
+
+	public static void setDivisionGames(ArrayList teams){
+		ArrayList div = new ArrayList<>();
+		div.add(teams.get(0));
+		for(int i = 1; i < teams.size() ; ++i){
+			if(i % 4 == 0){
+				setDivGame(div);
+				div.clear();
+			}
+			div.add(teams.get(i));
+		}
+		setDivGame(div);
+	}
+
+	public static void printArray(ArrayList teams){
+		for(int i = 0; i < teams.size() ; ++i){
+			Team a = (Team)teams.get(i);
+			System.out.println(a.getName());
+		}
+	}
+
+	public static void main (String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("What year do you want a schedule for? YYYY : ");
+		int year = sc.nextInt();
+		ArrayList<Team> teams = new ArrayList<Team>(32);
+		int prev = year - 1;
+	
+		// AFC teams
+		// AFC East
+		teams = init_teams("Buffalo","Bills", prev, sc, teams, Team.Confrence.AFC, Team.Division.East);
+		teams = init_teams("Miami","Dolphins", prev, sc, teams, Team.Confrence.AFC, Team.Division.East);
+		teams = init_teams("New England","Patroits", prev, sc, teams, Team.Confrence.AFC, Team.Division.East);
+		teams = init_teams("New York","Jets", prev, sc, teams, Team.Confrence.AFC, Team.Division.East);
+		// AFC North
+		teams = init_teams("Baltimore","Ravens", prev, sc, teams, Team.Confrence.AFC, Team.Division.North);
+		teams = init_teams("Cincinnatti","Bengals", prev, sc, teams, Team.Confrence.AFC, Team.Division.North);
+		teams = init_teams("CLeveland","Browns", prev, sc, teams, Team.Confrence.AFC, Team.Division.North);
+		teams = init_teams("Pittsburgh","Steelers", prev, sc, teams, Team.Confrence.AFC, Team.Division.North);
+		// AFC South
+		teams = init_teams("Houston","Texans", prev, sc, teams, Team.Confrence.AFC, Team.Division.South);
+		teams = init_teams("Jacksonville","Jaguars", prev, sc, teams, Team.Confrence.AFC, Team.Division.South);
+		teams = init_teams("Indianapolis","Colts", prev, sc, teams, Team.Confrence.AFC, Team.Division.South);
+		teams = init_teams("Tennessee","Titans", prev, sc, teams, Team.Confrence.AFC, Team.Division.South);
+		// AFC West
+		teams = init_teams("Denver","Broncos", prev, sc, teams, Team.Confrence.AFC, Team.Division.West);
+		teams = init_teams("Kansas City","Chiefs", prev, sc, teams, Team.Confrence.AFC, Team.Division.West);
+		teams = init_teams("Oakland","Raiders", prev, sc, teams, Team.Confrence.AFC, Team.Division.West);
+		teams = init_teams("San Diego","Chargers", prev, sc, teams, Team.Confrence.AFC, Team.Division.West);
+		// NFC teams
+		// NFC East
+		teams = init_teams("Dallas","Cowboys", prev, sc, teams, Team.Confrence.NFC, Team.Division.East);
+		teams = init_teams("New York","Giants", prev, sc, teams, Team.Confrence.NFC, Team.Division.East);
+		teams = init_teams("Philadelphia","Eagles", prev, sc, teams, Team.Confrence.NFC, Team.Division.East);
+		teams = init_teams("Washington","Redskins", prev, sc, teams, Team.Confrence.NFC, Team.Division.East);
+		// NFC North
+		teams = init_teams("Chicago","Bears", prev, sc, teams, Team.Confrence.NFC, Team.Division.North);
+		teams = init_teams("Detroit","Lions", prev, sc, teams, Team.Confrence.NFC, Team.Division.North);
+		teams = init_teams("Green Bay","Packers", prev, sc, teams, Team.Confrence.NFC, Team.Division.North);
+		teams = init_teams("Minnesota","Vikings", prev, sc, teams, Team.Confrence.NFC, Team.Division.North);
+		// NFC South
+		teams = init_teams("Atlanta","Falcons", prev, sc, teams, Team.Confrence.NFC, Team.Division.South);
+		teams = init_teams("Carolina","Panthers", prev, sc, teams, Team.Confrence.NFC, Team.Division.South);
+		teams = init_teams("New Orleans","Saints", prev, sc, teams, Team.Confrence.NFC, Team.Division.South);
+		teams = init_teams("Tampa Bay","Buccanners", prev, sc, teams, Team.Confrence.NFC, Team.Division.South);
+		// NFC West
+		teams = init_teams("Arizona","Cardinals", prev, sc, teams, Team.Confrence.NFC, Team.Division.West);
+		teams = init_teams("San Francisco","49ers", prev, sc, teams, Team.Confrence.NFC, Team.Division.West);
+		teams = init_teams("Seattle","Seahawks", prev, sc, teams, Team.Confrence.NFC, Team.Division.West);
+		teams = init_teams("St.Louis","Rams", prev, sc, teams, Team.Confrence.NFC, Team.Division.West);
+
+		setByeWeek(teams);
+		setDivisionGames(teams);
+		for(int i = 0 ; i < teams.size() ; ++i){
+			Team a = (Team)teams.get(i);
+			String [] aSch = a.getSchedule();
+			System.out.println(a.getName());
+			for (int j = 0 ; j < aSch.length ; ++j){
+	//			if(aSch[j] != null)
+				System.out.println("Week "+ (j+1) + " : " + aSch[j]);
+			}
+		}
+	}
+
+
+	/*
+		http://www.pro-football-reference.com/blog/?p=521
+		The rotation of intra-conference pairings is as follows:
+
+		2002: AFC West vs. NFC West, AFC North vs. NFC South, AFC South vs. NFC East, AFC East vs. NFC North
+		2003: AFC West vs. NFC North, AFC North vs. NFC West, AFC South vs. NFC South, AFC East vs. NFC East
+		2004: AFC West vs. NFC South, AFC North vs. NFC East, AFC South vs. NFC North, AFC East vs. NFC West
+		2005: AFC West vs. NFC East, AFC North vs. NFC North, AFC South vs. NFC West, AFC East vs. NFC South
+
+		The pattern then repeats starting in 2006.
+
+		For the inter-conference matchups between divisions, the following schedule is used to determine which divisions will face off each year:
+
+		2002: West versus East, North versus South
+		2003: West versus North, East versus South
+		2004: West versus South, North versus East
+
+		The pattern then repeats starting in 2005.
+	*/
+}
--- Team.java
+++ Team.java
@@ -6,7 +6,6 @@ import java.util.Random;
 class Team{
 	String name;
 	int division_standing;
-
 	public enum Confrence{
 		AFC, NFC
 	}
@@ -36,6 +35,14 @@ class Team{
 		this.schedule = sch;
 	}
 
+	public void printSchedule(){
+		System.out.println(this.getName());
+		for(int i = 0 ; i < this.schedule.length ; ++i){
+			int week = i + 1;
+			System.out.println("week " + week + ": " + this.schedule[i]);
+		}
+	}
+/*
 	public static Team returnObjectByName(ArrayList a,String name){
 		Team iter = null;
 		for(int i = 0 ; i < a.size() ; ++i){
@@ -43,6 +50,17 @@ class Team{
 			if(name.equals(iter.getName())) break;
 		}
 		return iter;
+	}
+
+	public static ArrayList init_teams(String city, String name, int prev, Scanner sc, ArrayList team, Confrence conf, Division div){
+		System.out.print("Where did the " +city + " "+ name + " finish in the " + conf +" "+ div + " in " + prev + " : ");
+		int stand = sc.nextInt();
+		while(stand < 1 || stand > 4){
+			System.out.print("Please enter a number between 1-4 where the "+name+" finished the season in "+prev+": ");
+			stand = sc.nextInt();
+		}
+		team.add(new Team(name, conf, div, stand));
+		return team;
 	}
 
 	public static void setByeWeek(ArrayList teams){
@@ -56,21 +74,102 @@ class Team{
 			Team t = returnObjectByName(teams, bye_selection.get(bye_team).getName());
 			bye_selection.remove(bye_team);
 			String [] s = t.getSchedule();
-			s[bye_week] = "Bye";
+			// array zero based, schedule one based
+			s[bye_week-1] = "Bye";
 			t.setSchedule(s);
-			System.out.println(t.getName() + " are on a bye on Week " + bye_week);
+			System.out.println(t.getName() + " are on a bye in Week " + bye_week);
 		}
 	}
 
-	public static ArrayList init_teams(String city, String name, int prev, Scanner sc, ArrayList team, Confrence conf, Division div){
-		System.out.print("Where did the " +city + " "+ name + " finish in the " + conf +" "+ div + " in " + prev + " : ");
-		int stand = sc.nextInt();
-		while(stand < 1 || stand > 4){
-			System.out.print("Please enter a number between 1-4 where the "+name+" finished the season in "+prev+": ");
-			stand = sc.nextInt();
+	public static void setGame(Team a , Team b, boolean isAHome, int week){
+		String [] aSch = a.getSchedule();
+		String [] bSch = b.getSchedule();
+		if(isAHome){
+			aSch[week] = b.getName();
+			bSch[week] = "@ " + a.getName();
+		} else {
+			aSch[week] = "@ " + b.getName();
+			bSch[week] = a.getName();
 		}
-		team.add(new Team(name, conf, div, stand));
-		return team;
+		a.setSchedule(aSch);
+		b.setSchedule(bSch);
+	}
+
+	public static void setDivGame(ArrayList div){
+		Random rand = new Random();
+		int divOpp = rand.nextInt(3) + 1;
+		System.out.println(divOpp);
+		if(divOpp == 1){
+			boolean home = rand.nextBoolean();
+			Team t0 = (Team)div.get(0);
+			Team t1 = (Team)div.get(1);
+			String [] sch0 = t0.getSchedule();
+			String [] sch1 = t1.getSchedule();
+			int returnTrip = rand.nextInt(16);
+			while(sch0[returnTrip] != null && sch1[returnTrip] != null) returnTrip = rand.nextInt(16);
+			setGame(t0, t1, home, 16);
+			setGame(t0, t1, !home, returnTrip);
+		} else if (divOpp == 2){
+			boolean home = rand.nextBoolean();
+			Team t0 = (Team)div.get(0);
+			Team t2 = (Team)div.get(2);
+			String [] sch0 = t0.getSchedule();
+			String [] sch2 = t2.getSchedule();
+			int returnTrip = rand.nextInt(16);
+			while(sch0[returnTrip] != null && sch2[returnTrip] != null) returnTrip = rand.nextInt(16);
+			setGame(t0, t2, home, 16);
+			setGame(t0, t2, !home, returnTrip);
+		} else {
+			boolean home = rand.nextBoolean();
+			Team t0 = (Team)div.get(0);
+			Team t3 = (Team)div.get(3);
+			String [] sch0 = t0.getSchedule();
+			String [] sch3 = t3.getSchedule();
+			int returnTrip = rand.nextInt(16);
+			while(sch0[returnTrip] != null && sch3[returnTrip] != null) returnTrip = rand.nextInt(16);
+			setGame(t0, t3, home, 16);
+			setGame(t0, t3, !home, returnTrip);
+		}
+	}
+
+	public static void setDivisionGames(ArrayList teams){
+		ArrayList divisions = new ArrayList<>();
+		ArrayList div = new ArrayList<>();
+		div.add(teams.get(0));
+		for(int i = 1; i < teams.size() ; ++i){
+		//	System.out.println(((Team)teams.get(i)).getName());
+			if(i % 4 == 0){
+				divisions.add(div);
+				for(int j = 0 ; j < divisions.size() ; ++j){
+					ArrayList a = (ArrayList)divisions.get(j);
+					System.out.println("BEFORE!");
+					for(int k = 0 ; k < a.size(); ++k){
+						System.out.println(((Team)a.get(k)).getName());
+					}
+					System.out.println("AFTER!");
+				}
+				div.clear();
+			}
+			div.add(teams.get(i));
+		}
+	 	divisions.add(div);
+	 	System.out.println(divisions.size());
+		for(int i = 0 ; i < divisions.size() ; ++i) {
+			ArrayList d = (ArrayList)divisions.get(i);
+			System.out.println(d.size());
+		//	setDivGame(d);
+			for(int j = 0 ; j < d.size() ; ++j){
+				Team a = (Team)d.get(j);
+				String [] sch = a.getSchedule();
+				System.out.println(a.getName());
+		//		for(int k = 0 ; k < sch.length ; ++k){
+		//			if(sch[k] != null){
+		//				System.out.println("Week " + (k+1) + ": " + sch[k]);
+		//			}
+		//		}
+			}
+		}	
+
 	}
 
 	public static void printArray(ArrayList teams){
@@ -131,5 +230,7 @@ class Team{
 		teams = init_teams("St.Louis","Rams", prev, sc, teams, Confrence.NFC, Division.West);
 
 		setByeWeek(teams);
+		setDivisionGames(teams);
 	}
+	*/
 }
